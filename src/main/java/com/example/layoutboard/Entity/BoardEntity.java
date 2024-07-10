f74267b6d070a1d8b65416005906dbae9db48702
SeonukJang <seonuk656@gmail.com>
2024-07-10 12:52:48 +0900

    2. 테이블 설계

--- src/main/java/com/example/layoutboard/Entity/BoardEntity.java
+++ src/main/java/com/example/layoutboard/Entity/BoardEntity.java
@@ -1,4 +1,25 @@
 package com.example.layoutboard.Entity;
 
-public class BoardEntity {
+import jakarta.persistence.*;
+import lombok.*;
+
+//개발자가 설계한 테이블을 가지고 작업
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Builder
+@Entity
+@Table(name="board")
+public class BoardEntity extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name="id")
+    private Integer id; //일련번호
+    @Column(name="subject", length = 100, nullable = false)
+    private String subject; //제목
+    @Column(name="subject", length = 200)
+    private String content; //내용
+    //length를 이용해서 길이의 제약, input태그에서 같이 제약
 }
--- src/main/java/com/example/layoutboard/Util/PaginationUtil.java
+++ src/main/java/com/example/layoutboard/Util/PaginationUtil.java
@@ -1,4 +1,35 @@
 package com.example.layoutboard.Util;
 
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
 public class PaginationUtil {
+    //페이지처리 메소드
+    public static Map<String, Integer> Pagenation(Page<?> page) {
+        Map<String, Integer> pageInfo = new HashMap<>();
+
+        //변수명 사용자 마음대로, put 고정
+        int currentPage = page.getNumber()+1;
+        int totalPage = page.getTotalPages();
+        int blockLimit = 5;
+
+        //필요한 페이지 정보를 만들기 (java.Math - 수학라이브러리)
+        int startPage = Math.max(1, currentPage-blockLimit/2);
+        int endPage = Math.min(startPage+blockLimit-1, totalPage);
+        int prevPage = Math.max(1, currentPage-1); //currentPage-10
+        int nextPage = Math.min(totalPage, currentPage+1); //2->3, currentPage+10 2->12
+        int lastPage = totalPage;
+
+        pageInfo.put("startPage", startPage);
+        pageInfo.put("endPage", endPage);
+        pageInfo.put("prevPage", prevPage);
+        pageInfo.put("nextPage", nextPage);
+        pageInfo.put("lastPage", lastPage);
+
+        return pageInfo;
+    }
 }
